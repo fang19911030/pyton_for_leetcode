3526d6b6156b7a79b857db078a119940fd56ef54
fang19911030 <fang291831388@gmail.com>
2016-11-13 16:26:41 -0500

    Same as Hexadecimal conversion

---
+++ excel_sheet_column_number171.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 13 16:16:32 2016
+
+@author: fang2
+"""
+
+def titleToNumber(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    length=len(s)
+    base=ord('A')
+    result=0
+    print (length)
+    for i in range(length):
+        print (i)
+        result+=(ord(s[i])-base+1)*(26**(length-i-1))
+        
+    return result
+    
+s="A"
+s2="AA"
+print (titleToNumber(s))
+print (titleToNumber(s2))
